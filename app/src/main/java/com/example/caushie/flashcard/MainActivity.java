4a58429bafe55c3817f592d9c3606068a252986f
Endi <mrendi29@gmail.com>
2018-10-15 00:35:43 -0400

    New Options Added

--- app/src/main/java/com/example/caushie/flashcard/MainActivity.java
+++ app/src/main/java/com/example/caushie/flashcard/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.caushie.flashcard;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    boolean isShowingAnswers = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,23 +16,24 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.flashcard_answer).setBackgroundColor(getResources().getColor(R.color.green));
+
+                findViewById(R.id.flashcard_answer).setBackgroundResource(R.drawable.drawable_correct);
             }
         });
         // If user guesses the Answer incorrectly the correct answer is filled green and incorrect answer is filled red.
         findViewById(R.id.answer1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.answer1).setBackgroundColor(getResources().getColor(R.color.red));
-                findViewById(R.id.flashcard_answer).setBackgroundColor(getResources().getColor(R.color.green));
+                findViewById(R.id.answer1).setBackgroundResource(R.drawable.drawable_incorect);
+                findViewById(R.id.flashcard_answer).setBackgroundResource(R.drawable.drawable_correct);
             }
         });
         // If user guesses the Answer incorrectly the correct answer is filled green and incorrect answer is filled red.
         findViewById(R.id.answer2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.answer2).setBackgroundColor(getResources().getColor(R.color.red));
-                findViewById(R.id.flashcard_answer).setBackgroundColor(getResources().getColor(R.color.green));
+                findViewById(R.id.answer2).setBackgroundResource(R.drawable.drawable_incorect);
+                findViewById(R.id.flashcard_answer).setBackgroundResource(R.drawable.drawable_correct);
             }
         });
         // Reset the colors.
@@ -38,35 +41,38 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.rootview).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.answer2).setBackgroundColor(getResources().getColor(R.color.yellow));
-                findViewById(R.id.answer1).setBackgroundColor(getResources().getColor(R.color.yellow));
-                findViewById(R.id.flashcard_answer).setBackgroundColor(getResources().getColor(R.color.yellow));
+                findViewById(R.id.answer2).setBackgroundResource(R.drawable.card_answer);
+                findViewById(R.id.answer1).setBackgroundResource(R.drawable.card_answer);
+                findViewById(R.id.flashcard_answer).setBackgroundResource(R.drawable.card_answer);
 
 
             }
         });
+
         // Answers Hide and Eye icon Changes
+        // boolean is used to know when to show and hide answers.
+        // false and true values are passed between the if and else statement to achieve a " loop" effect
         findViewById(R.id.toggle_choices_visibility).setOnClickListener(new View.OnClickListener() {
         @Override
             public void onClick(View v) {
-                findViewById(R.id.answer1).setVisibility(View.INVISIBLE);
-                findViewById(R.id.answer2).setVisibility(View.INVISIBLE);
-                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
-                findViewById(R.id.toggle_choices_invisibility).setVisibility(View.VISIBLE);
+                if (isShowingAnswers) {
+                    findViewById(R.id.answer1).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.answer2).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                    ((ImageView) findViewById(R.id.toggle_choices_visibility)).setImageResource(R.drawable.eye_off);
+                    isShowingAnswers=false;
+            }
+            else {
+                    findViewById(R.id.answer1).setVisibility(View.VISIBLE);
+                    findViewById(R.id.answer2).setVisibility(View.VISIBLE);
+                    findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                    ((ImageView) findViewById(R.id.toggle_choices_visibility)).setImageResource(R.drawable.eye_on);
+                    isShowingAnswers=true;
+                }
             }
         });
-        
-        // Answers reappear and icon turns default
-        findViewById(R.id.toggle_choices_invisibility).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                findViewById(R.id.answer1).setVisibility(View.VISIBLE);
-                findViewById(R.id.answer2).setVisibility(View.VISIBLE);
-                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
-                findViewById(R.id.toggle_choices_invisibility).setVisibility(View.INVISIBLE);
 
-                }
-        });
+
 
 
 
